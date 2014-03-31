36a3cf2e1f6a35c0d9f6af36bd768575e3ad78e9
Shinmera <shinmera@tymoon.eu>
2014-03-31 17:01:02 +0200

    Updated docstrings, fixed return values.

--- pipeline.lisp
+++ pipeline.lisp
@@ -53,19 +53,30 @@ As secondary value it returns the position of the matched item within the parent
   (:method ((pipeline pipeline) (name string))
     (find-parent pipeline (gethash name (names pipeline)))))
 
-(defgeneric insert (item place &optional position)
+(defgeneric insert (segment place &optional position)
   (:documentation "Appends the item to the given place unless a specific position is given.
 If position is set, the item is expected to be inserted at the specified position, without
-replacing or disturbing any other items.")
-  (:method (item (array array) &optional position)
+replacing or disturbing any other items. 
+
+Returns the segment.
+
+Do note that using insert directly can affect names and make them invalid or point to unexpected
+segments. You should always use add-segment or insert-segment instead if possible.")
+  (:method (segment (array array) &optional position)
     (if position
-        (vector-push-extend-position item array position)
-        (vector-push-extend item array))))
+        (vector-push-extend-position segment array position)
+        (vector-push-extend segment array))
+    segment))
 
 (defgeneric withdraw (place &optional position)
   (:documentation "Removes an item from the place, by default from the end unless position is set.
 If position is given, it is expected that the specified item is removed without disturbing any
-other items and without leaving any empty spaces within the parent.")
+other items and without leaving any empty spaces within the parent. 
+
+Returns the segment.
+
+Do note that using withdraw directly can affect names and make them invalid or point to unexpected
+segments. You should always use remove-place instead if possible.")
   (:method ((array array) &optional position)
     (if position
         (vector-pop-position array position)
@@ -75,30 +86,36 @@ other items and without leaving any empty spaces within the parent.")
   (:documentation "Add a new segment to the pipeline.
 If place is set, the pipe is added to the specified place as per INSERT.
 The place specified is expected to be a splitter or similar to append
-the pipe to.")
+the pipe to. Returns the segment.")
   (:method ((pipeline pipeline) (segment segment) &optional place)
     (insert segment (find-place pipeline place)))
   (:method ((pipeline pipeline) (array array) &optional place)
     (insert array (find-place pipeline place)))
   (:method ((pipeline pipeline) (array (eql :pipe)) &optional place)
     (insert (make-pipe) (find-place pipeline place))))
-
+;; FIXME: Removing before will destroy place names.
 (defgeneric remove-place (pipeline place)
   (:documentation "Removes the given place (as well as its children, if any).
-This also removes any names that either match or go through the specified place.")
+This also removes any names that either match or go through the specified place
+and adapts names that would be affected by a place shift. 
+
+Returns the segment.")
   (:method ((pipeline pipeline) place)
-    (multiple-value-bind (parent pos) (find-parent pipeline place)
-      (withdraw parent pos))
-    (loop for k being the hash-keys of (names pipeline)
-          for v being the hash-values of (names pipeline)
-          when (and (<= (length place) (length v))
-                    (every #'= place v))
-            do (remhash k (names pipeline)))))
+    (prog1
+        (multiple-value-bind (parent pos) (find-parent pipeline place)
+          (withdraw parent pos))
+      (loop for k being the hash-keys of (names pipeline)
+            for v being the hash-values of (names pipeline)
+            when (and (<= (length place) (length v))
+                      (every #'= place v))
+              do (remhash k (names pipeline))))))
 ;; FIXME: Inserting before will destroy place names.
 (defgeneric insert-segment (pipeline segment place)
   (:documentation "Insert the segment at the given place.
 Note that the segment is always inserted into the parent as specified by the place
-and found by FIND-PARENT and inserted into the position as per INSERT.")
+and found by FIND-PARENT and inserted into the position as per INSERT. 
+
+Returns the segment.")
   (:method ((pipeline pipeline) (segment segment) place)
     (multiple-value-bind (parent pos) (find-parent pipeline place)
       (insert segment parent pos)))
@@ -112,7 +129,9 @@ and found by FIND-PARENT and inserted into the position as per INSERT.")
   (:documentation "Replace a place with a pipe.
 This happens simply through REMOVE-PLACE and INSERT-PIPE.
 
-Note that this will destroy names due to REMOVE-PLACE.")
+Note that this will destroy names due to REMOVE-PLACE.
+
+Returns the segment.")
   (:method ((pipeline pipeline) place (segment segment))
     (remove-place pipeline place)
     (insert-segment pipeline segment place))
@@ -125,21 +144,27 @@ Note that this will destroy names due to REMOVE-PLACE.")
 (defgeneric move-place (pipeline place new-place)
   (:documentation "Moves a place to another while preserving names.
 This attempts to fix names associated with the place or deeper places
-by changing their place as well.")
+by changing their place as well.
+
+Returns the segment.")
   (:method ((pipeline pipeline) place new-place)
-    (multiple-value-bind (parent pos) (find-parent pipeline place)
-      (let ((segment (withdraw parent pos)))
-        (insert-segment pipeline segment new-place)))
-    (loop for k being the hash-keys of (names pipeline)
-          for v being the hash-values of (names pipeline)
-          when (and (<= (length place) (length v))
-                    (every #'= place v))
-            do (set-name pipeline (append place (subseq v (length place))) k))))
+    (prog1
+        (multiple-value-bind (parent pos) (find-parent pipeline place)
+          (let ((segment (remove-place pipeline place)))
+            (insert-segment pipeline segment new-place)))
+      (loop for k being the hash-keys of (names pipeline)
+            for v being the hash-values of (names pipeline)
+            when (and (<= (length place) (length v))
+                      (every #'= place v))
+              do (set-name pipeline (append place (subseq v (length place))) k)))))
 
 (defgeneric set-name (pipeline place name)
-  (:documentation "Associates a place with a name so it can be accessed more easily.")
+  (:documentation "Associates a place with a name so it can be accessed more easily.
+
+Returns the name.")
   (:method ((pipeline pipeline) (place list) name)
-    (setf (gethash name (names pipeline)) place)))
+    (setf (gethash name (names pipeline)) place)
+    name))
 
 (defgeneric pass (segment message)
   (:documentation "Pass a message through a segment.
@@ -151,12 +176,15 @@ return NIL, passing stops. This does not affect passing in
 segments on other splitters. Do note that changing the object
 itself directly will of course als be reflected at any other
 point in the pipeline passing, so muting the passed object
-should be avoided wherever possible.")
+should be avoided wherever possible.
+
+Returns the message.")
   (:method ((pipeline pipeline) message)
     (pass (pipeline pipeline) message))
   (:method ((array array) message)
     (loop for item across array
           do (setf message (pass item message))
-          while message))
+          while message)
+    message)
   (:method ((segment segment) message)
     message))
