788dd45de3135c2d0d1314744ee2180841de112a
Shinmera <shinmera@tymoon.eu>
2016-12-26 16:20:58 +0100

    Improvement to print object methods

--- pipe.lisp
+++ pipe.lisp
@@ -10,19 +10,19 @@
   (:documentation "Base segment class."))
 
 (defmethod print-object ((segment segment) stream)
-  (format stream "[PIPE]" )
+  (format stream "[~a]" (type-of segment))
   segment)
 
 (defclass filter (segment) ()
   (:documentation "Base filter class."))
 
 (defmethod print-object ((filter filter) stream)
-  (format stream ":FILTER:")
+  (format stream ":~a:" (type-of filter))
   filter)
 
 (defclass faucet (segment) ()
   (:documentation "Base faucet class."))
 
 (defmethod print-object ((faucet faucet) stream)
-  (format stream ">>FAUCET")
+  (format stream ">>~a" (type-of faucet))
   faucet)
